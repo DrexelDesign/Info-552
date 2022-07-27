e85a773e2147102952b0b044643450993feaaa5d
Josh <joshGdovindesigns.com>
2022-07-27 08:35:42 -0400

    updated working projeect

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,48 +10,7 @@ import { displayWindowSize } from "./mobileResizing"
 let burgerClicked = false;
 let burgerButton = document.querySelector("#burger");
 
-// window.addEventListener('load', function () {
-//     skillsTrigger();
-//     galleryAni();
-// })
-
-// burgerButton.addEventListener("click", () => {
-//     if(burgerClicked === false){
-//         burgerTL.play();
-//         menuAnimation.play();
-//         burgerClicked = true;
-//     }else{
-//         burgerTL.reverse();
-//         menuAnimation.reverse();
-//         burgerClicked = false;
-//     }
-// });
-
 burgerButton.addEventListener("click", openCloseMenu);
-
-// let navButtons = document.querySelectorAll(".nav-btns");
-
-// for(var i = 0; i < navButtons.length; i++){
-//     navButtons[i].addEventListener("click", openCloseMenu);
-// }
-
-// for (const button of navButtons) {
-//     button.addEventListener("click", checkScrolling);
-// }
-
-// var index = [].indexOf.call(navButtons);
-// for(const test of navButtons){
-//     test.addEventListener("click", testIndex);
-// }
-
-// function testIndex(e){
-//     // var index = [...this.children].indexOf(e.target);
-//     // console.log([].indexOf.call(navButtons, e.target));
-
-//     const indexValue = [].indexOf.call(navButtons, e.target)
-//     // scrollPage(indexValue);
-//     // console.log(index);
-// }
 
 // console.log(navButtons);
 function openCloseMenu() {
@@ -65,15 +24,6 @@ function openCloseMenu() {
         burgerClicked = false;
     }
 }
-
-// function checkScrolling(e) {
-//     // check to see which button was clicked
-//     const indexValue = [].indexOf.call(navButtons, e.target)
-//     if (indexValue != -1) {
-//         scrollPage(indexValue);
-//     }
-// }
-
 //listen to window resize for the menu being down and if it is force it back up
 window.addEventListener("resize", function(){
     if(burgerClicked === true){
@@ -87,3 +37,22 @@ window.addEventListener("resize", displayWindowSize);
 
 //check the window size on load
 window.addEventListener('load', displayWindowSize); 
+
+
+// $("#clipboard-btn").on("click", function () {
+var clipBtn = document.querySelector("#clipboard-btn");
+clipBtn.addEventListener("click",copyFunc);
+
+function copyFunc(){
+    var copyTextarea = document.querySelector('.js-email');
+    copyTextarea.focus();
+    copyTextarea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+}
